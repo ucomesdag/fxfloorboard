dde66167a7fd3563b0c5b370706fbc34fd71481a
Uco Mesdag <ucomesdag@gmail.com>
2006-11-14 21:49:13 +0000

    Re: Initial Project Upload

---
+++ bankTreeList.cpp
@@ -0,0 +1,145 @@
+/****************************************************************************
+**
+** Copyright (C) 2005-2006 Uco Mesdag. All rights reserved.
+**
+** This file is part of "GT-8: FX Floor Board".
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License along
+** with this program; if not, write to the Free Software Foundation, Inc.,
+** 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+**
+****************************************************************************/
+
+#include <QTreeWidget>
+#include <QLayout>
+#include "bankTreeList.h"
+
+bankTreeList::bankTreeList(QWidget *parent)
+    : QWidget(parent)
+{
+	QPalette pal;
+    pal.setColor(QPalette::Base,QColor(0,1,62));
+    pal.setColor(QPalette::Text,QColor(0,255,204));
+	pal.setColor(QPalette::Highlight,QColor(0,1,62));
+	pal.setColor(QPalette::HighlightedText,QColor(0,255,204));
+
+	//pal.setColor(QPalette::Window,QColor(Qt::red));
+	//pal.setColor(QPalette::WindowText,QColor(0,255,204));	//List Border
+	//pal.setColor(QPalette::Button,QColor(0,1,62));
+	//pal.setColor(QPalette::ButtonText,QColor(0,255,204));
+
+	//pal.setColor(QPalette::Light,QColor(Qt::red));				//Lighter than Button color.
+	//pal.setColor(QPalette::Midlight,QColor(Qt::red));			//Between Button and Light.
+	//pal.setColor(QPalette::Dark,QColor(Qt::gray));				//Darker than Button. (Dots)
+	//pal.setColor(QPalette::Mid,QColor(0,1,62));					//Between Button and Dark.
+	//pal.setColor(QPalette::Shadow,QColor(Qt::red));
+
+	QFont font;
+	font.setFamily("Arial");
+	font.setBold(true);
+	font.setPixelSize(10);
+	font.setStretch(120);
+
+	QTreeWidget *treeList = new QTreeWidget(this);
+	treeList->setColumnCount(1);
+	treeList->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+	treeList->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+	treeList->setPalette(pal);
+	treeList->setFont(font);
+
+	QStringList headers;
+    headers << "Boss GT-8";
+    treeList->setHeaderLabels(headers);
+
+
+	QTreeWidgetItem *user = new QTreeWidgetItem(treeList);
+	user->setText(0, "User");
+	user->setWhatsThis(0, "User Banks");
+	//user->setIcon(...);
+
+    QList<QTreeWidgetItem *> userBankRanges;
+    for (int a=1; a<=35; a++)
+	{
+		QTreeWidgetItem* bankRange = new QTreeWidgetItem; // don't pass a parent here!
+		bankRange->setText(0, QString::QString("Bank ").append(QString::number(a, 10)).append(" - ").append(QString::number(a+4, 10)) );
+		bankRange->setWhatsThis(0, "");
+		//bankRange->setIcon(...);
+
+		for (int b=a; b<=(a+4); b++)
+		{
+			QTreeWidgetItem* bank = new QTreeWidgetItem(bankRange);
+			bank->setText(0, QString::QString("Bank ").append(QString::number(b, 10)));
+			bank->setWhatsThis(0, "");
+			//bank->setIcon(...);
+
+			for (int c=1; c<=4; c++)
+			{
+				QTreeWidgetItem* patch = new QTreeWidgetItem(bank);
+				patch->setText(0, QString::QString("Patch ").append(QString::number(c, 10)));
+				patch->setWhatsThis(0, "");
+				//patch->setIcon(...);
+			};
+		};
+		userBankRanges << bankRange;
+		a += 4;
+	};
+	user->addChildren(userBankRanges);
+
+
+	QTreeWidgetItem *preset = new QTreeWidgetItem(treeList);
+	preset->setText(0, "Preset");
+	preset->setWhatsThis(0, "Preset Banks");
+	//user->setIcon(...);
+
+    QList<QTreeWidgetItem *> presetBankRanges;
+    for (int a=36; a<=85; a++)
+	{
+		QTreeWidgetItem* bankRange = new QTreeWidgetItem; // don't pass a parent here!
+		bankRange->setText(0, QString::QString("Bank ").append(QString::number(a, 10)).append(" - ").append(QString::number(a+4, 10)) );
+		bankRange->setWhatsThis(0, "");
+		//bankRange->setIcon(...);
+
+		for (int b=a; b<=(a+4); b++)
+		{
+			QTreeWidgetItem* bank = new QTreeWidgetItem(bankRange);
+			bank->setText(0, QString::QString("Bank ").append(QString::number(b, 10)));
+			bank->setWhatsThis(0, "");
+			//bank->setIcon(...);
+
+			for (int c=1; c<=4; c++)
+			{
+				QTreeWidgetItem* patch = new QTreeWidgetItem(bank);
+				patch->setText(0, QString::QString("Patch ").append(QString::number(c, 10)));
+				patch->setWhatsThis(0, "");
+				//patch->setIcon(...);
+			};
+		};
+		presetBankRanges << bankRange;
+		a += 4;
+	};
+	preset->addChildren(presetBankRanges);
+
+	treeList->setExpanded(treeList->model()->index(0, 0), true);
+	treeList->setExpanded(treeList->model()->index(1, 0), true);
+
+	QVBoxLayout *treeListLayout = new QVBoxLayout;
+	treeListLayout->addWidget(treeList);
+	treeListLayout->setMargin(0);
+	treeListLayout->setSpacing(0);
+	setLayout(treeListLayout);
+};
+
+void bankTreeList::updateSize(QRect newrect)
+{
+	this->setGeometry(newrect);
+};
